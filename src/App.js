68fe17261b5d1dfda2b4eb661fec0154ca01c115
Alekxander Baxwill <abax@itu.dk>
2020-12-30 12:10:00 +0100

    first - buggy timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,161 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Timer from './Components/Timer';
 
-function App() {
+var App = () => {
+  const [cityInp, setCityInp] = useState("");
+  const [sendVal, setSendVal] = useState("");   //final val being sent
+  const [score, setScore] = useState(0);
+  const [acc, setAcc] = useState(false);    //if sent city was accepted
+
+  // const [isLoading, setLoading] = useState(true);
+  const [isDone, setIsDone] = useState(false);   //is game-timer done, ie. is can user send cities
+  const [startCh, setStartCh] = useState('Æ') //useState(String.fromCharCode(Math.floor(Math.random() * ("Z".charCodeAt(0) - "A".charCodeAt(0) + 1)) + "A".charCodeAt(0)));
+  const [usedCities, setUsedCities] = useState(["Præstø","Pedersker","Padborg","Pjedsted","Pindstrup","Pårup","Præstbro","Pandrup","Poulstrup"]); //useState([]);
+  const [infoMess, setInfoMess] = useState("");
+  // const [citiesByStartCh, setCitiesByStartCh] = useState([]);
+
+  const handleChange = (event) => {
+    setCityInp(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();   //prev sit from reloading..
+    setSendVal(cityInp);
+    const cityInpLow = cityInp.toLowerCase();
+    isStartChValid('W');  //test
+    if ((startCh.toLowerCase() === cityInpLow.charAt(0) && !usedCities.includes(cityInpLow))) {
+      checkCity(cityInp);
+    } else {
+      setAcc(false);
+    }
+  }
+
+  const getAll = () => {
+    axios.get('https://localhost:44334/city')
+      .then(res => {
+        console.log(res.data);
+        setScore(res.data);
+        // debugger;
+      }).catch((error) => {
+        console.log(error);
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      })
+    }
+
+  //passed down to Timer - reset all state
+  function stopGame () {
+    setIsDone(!isDone);
+    if(isDone) {
+      setScore(0);
+      setUsedCities([]);
+    }
+  }
+
+  const checkCity = (cityName) => {
+    axios.get('https://localhost:5001/city/' + cityName)
+      .then(res => {
+        console.log("checkCity:", res.data);
+        // setLoading(false);
+        setAcc(res.data);
+        if(res.data) {
+          setCityInp("");
+          setScore(score + 1);
+          setUsedCities(oldLst => [...oldLst, cityName.toLowerCase()]);
+          // setStartCh(cityName.charAt(cityName.length - 1));
+          isStartChValid(cityName.charAt(cityName.length - 1));   //recurse until accep. new rand startCh
+
+          // console.log(cityName.charAt(cityName.length - 1));
+        }
+    });
+  }
+
+  //recurses until new usable startCh is generated - then changes state
+  const isStartChValid = (stCh) => {
+    axios.get('https://localhost:5001/cat/' + stCh.toUpperCase())
+    .then(resp =>  {
+        const citiesByCh = resp.data.citiesStartCh;
+        console.log(stCh+" startCh: ", citiesByCh);
+        debugger;
+        if (citiesByCh.length > 0) {
+          const fil = usedCities.filter(name => citiesByCh.includes(name));   //keep items that are in both lsts
+          if (fil.length === citiesByCh.length) {   //lsts identical - thus all cities w. that startCh are used
+            setInfoMess("Random letter generated \n No cities left starting with: \n" + stCh);
+            setTimeout(() => {
+              setInfoMess("")
+            }, 500000)
+            isStartChValid(randStartCh());
+
+          } else {
+            setStartCh(stCh);
+          }
+        } else {  //no cities w. startCh in API e.g 'Z' - find new rand. startCh
+          debugger;
+          // isStartChValid(randStartCh());
+          isStartChValid('P');
+
+        }
+    })
+  }
+
+
+  const randStartCh = () => {
+    const randAZLst = [...Array('Z'.charCodeAt(0) - 'A'.charCodeAt(0) + 1).keys()].map(i => i + 'A'.charCodeAt(0));   //[65 .. 90]
+    // console.log("randAZ:", randAZLst);
+    const randLst = randAZLst.concat(['Æ'.charCodeAt(0), 'Ø'.charCodeAt(0), 'Å'.charCodeAt(0)]);
+    // console.log("ÆØÅ", randLst);
+    const res = randLst[Math.floor(Math.random() * randLst.length)];
+
+    return String.fromCharCode(res);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+
+      <header>
+        Header
       </header>
+      <main className="main">
+        <img className="bg" alt="nyhavn" src="https://images.unsplash.com/photo-1513622470522-26c3c8a854bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"/>
+
+        <aside className="left">
+          Left
+        </aside>
+        <article className="mainContent">
+          MainContent
+          <h2>City Name Challenge - Denmark</h2>
+          <p>Enter a danish City that starts with the letter: </p>
+          <h2>{startCh.toUpperCase()}</h2>
+          {sendVal ? <p> Entered: <span className={acc ? "correctTxt" : "wrongTxt" }>{sendVal}</span></p> : <p></p> }
+          {infoMess.length === 0 ? <p>Score: {score}</p> : <p className={["display-linebreak", "blue-border"].join(" ")}>{infoMess}</p>}
+          <Timer secs={60} stopFunc={stopGame}/>
+          {!isDone ?
+            <div>
+              <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={cityInp} />
+                <button type="submit">Submit city</button>
+              </form>
+            </div>
+            : <p>Game done</p>
+          }
+        </article>
+
+        <nav className="right">
+          RIGHT
+        </nav>
+
+      </main>
+
+
+
+     <footer className="footer">Footer</footer>
     </div>
+
   );
 }
 
---
+++ src/Components/Timer.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+var Timer = (props) => {
+
+    const [seconds, setSeconds] = useState(props.secs);
+    const [isRunning, setIsRunning] = useState(false);
+
+    const toggleIsRunning = () => { setIsRunning(!isRunning); }
+    const reset = () => {
+        setSeconds(props.secs);
+        setIsRunning(false);
+        props.stopFunc();   //reset game
+    }
+
+    useEffect(() => {
+        let interval = null;
+        if (isRunning && seconds > 0) {
+            interval = setInterval(() => {
+                // setSeconds(seconds => seconds - 1);
+                setSeconds(seconds-1);
+              }, 1000);
+        } else if (isRunning) {
+            // debugger;
+            setSeconds(0);
+            setIsRunning(false);
+            props.stopFunc();
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning, seconds]);     //Only re-run effect if these state changes
+
+    return(
+        <div className="Timer">
+            {seconds}s
+            <button onClick={toggleIsRunning}>{isRunning ? "Pause" : "Start"} </button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+}
+
+
+export default Timer;
